e34d2da10ea506e6b63fc51c1d9bd282af3aad01
Nouman Altaf <nouman@markinson.com.au>
2025-03-15 15:17:44 +0300

    added google verification meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <meta name="google-site-verification" content="_lP3fYRfZyYaoeXfHOAOPIC1-0tY8pEPkNXEf3NiyOw" />
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
